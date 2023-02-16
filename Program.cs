840e31b885b269b811e73d81abe3c8c1f711dc14
Wei Da Chen <43672339+chenweida6220@users.noreply.github.com>
2023-02-15 19:30:19 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace WebAPIClient
+{
+    class ZipCodeLookup
+    {
+        [JsonProperty("zipcode")]
+        public int ZipCode  { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("countryabbreviation")]
+        public string CountryAbbreviation { get; set; }
+
+        [JsonProperty("places")]
+        public string Places { get; set; }
+    }
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            await ProcessRepositories();
+        }
+
+        private static async Task ProcessRepositories()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter ZipCode: ");
+                    int postalcode = Convert.ToInt32(Console.ReadLine());
+                    if (postalcode == 0)
+                    {
+                        break;
+                    }
+
+                    var result = await client.GetAsync("http://api.zippopotam.us/us/" + postalcode);
+                    var resultRead = await result.Content.ReadAsStringAsync();
+
+                    var postalcode_deserialize = JsonConvert.DeserializeObject<ZipCodeLookup>(resultRead);
+                    Console.WriteLine("--------");
+                    Console.WriteLine("Country: " + postalcode_deserialize.Country + "  Zip Code: " + postalcode_deserialize.ZipCode);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Error not valid");
+                }
+            }
+        }
+    }
+}
